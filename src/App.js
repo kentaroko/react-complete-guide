db0c85b466e13182d434be4f096820665ee733ac
Ken Chen <ken.chen@taroko.io>
2019-04-26 11:03:29 +0800

    clean up assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-// import React, { useState } from 'react';
 import classes from './App.css';
-
 import Person from './Person/Person';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
-import Validation from './Validation/Validation';
-import CharBlock from './Char/Char';
 
 class App extends Component {
   state = {
@@ -16,9 +10,7 @@ class App extends Component {
       { id: '789', name: 'Staphine', age: 26 }
     ],
     otherState: 'some other value',
-    userName: 'ken chen',
     showPersons: false,
-    input: '',
   };
 
   deletePersonHandler = (personIndex) => {
@@ -26,23 +18,6 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
-  };
-
-  deleteCharBlock = (charIndex) => {
-    const input = this.state.input.split('');
-
-    input.splice(charIndex, 1);
-
-    const updatedInput = input.join('');
-
-    this.setState({input: updatedInput});
-  }
-
-
-  switchUserNameHandler = () => {
-    this.setState({
-      userName: 'ken cindy'
-    });
   };
 
   nameChangedHandler = (event, id) => {
@@ -53,7 +28,6 @@ class App extends Component {
       ...this.state.persons[personIndex]
     };
 
-    console.log(person);
     // const person = Object.assign({}, this.state.persons[personIndex]);
     person.name = event.target.value;
     const persons = [...this.state.persons];
@@ -64,12 +38,6 @@ class App extends Component {
     this.setState({persons: persons});
   };
 
-  userNameChangedHandler = (event) => {
-    this.setState({
-      userName: event.target.value
-    });
-  };
-
   togglePresonsHandler = () => {
     const doesShow = this.state.showPersons
     this.setState({
@@ -77,51 +45,9 @@ class App extends Component {
     });
   };
 
-  inputChangedHandler = (event) => {
-    this.setState({
-      input: event.target.value
-    });
-  };
-
   render() {
     let persons = null;
-    let first_assignment = null;
-    let chars = null;
-
     let btnClass = '';
-
-    if (false) {
-      first_assignment = (
-        <div>
-          <UserOutput
-            click={this.switchUserNameHandler}
-            name={this.state.userName}
-          >
-          </UserOutput>
-          <UserOutput name={this.state.userName}>
-          </UserOutput>
-          <UserInput
-            userName={this.state.userName}
-            changed={this.userNameChangedHandler}
-          >
-          </UserInput>
-        </div>
-      );
-    }
-
-    chars = (
-      <div>
-        {this.state.input.split('').map((char, index) => {
-          return (
-            <CharBlock
-              text={char}
-              key={index}
-              click={() => this.deleteCharBlock(index)}
-            />
-          )
-        })}
-      </div>
-    );
 
     if (this.state.showPersons) {
       persons = (
@@ -156,11 +82,6 @@ class App extends Component {
           className={btnClass}
           onClick={this.togglePresonsHandler}>Toggle Persons</button>
         {persons}
-        {first_assignment}
-        <input type="text" onChange={this.inputChangedHandler} value={this.state.input}/>
-        <p>{this.state.input.length}</p>
-        <Validation length={this.state.input.length}/>
-        {chars}
       </div>
     );
   };
@@ -168,48 +89,3 @@ class App extends Component {
 
 export default App;
 
-// const App = props => {
-//   const [personsState, setPersonsState] = useState({
-//     persons: [
-//       { name: 'Max', age: 28 },
-//       { name: 'Manu', age: 29 },
-//       { name: 'Stephanie', age: 26 }
-//     ]
-//   });
-//
-//   const [otherState, setOtherState] = useState('abc');
-//
-//   console.log(personsState, otherState);
-//
-//
-//   const switchNameHandler = () => {
-//     setPersonsState({
-//       persons: [
-//         { name: 'Maximilian', age: 28 },
-//         { name: 'Manu', age: 29 },
-//         { name: 'Stephanie', age: 27 }
-//       ]
-//     });
-//   };
-//
-//   return (
-//     <div className="App">
-//       <h1>Hi, I'm a React App</h1>
-//       <p>This is really working!</p>
-//       <button onClick={switchNameHandler}>Switch Name</button>
-//       <Person
-//         name={personsState.persons[0].name}
-//         age={personsState.persons[0].age}
-//       />
-//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>
-//         My Hobbies: Racing
-//       </Person>
-//       <Person
-//         name={personsState.persons[2].name}
-//         age={personsState.persons[2].age}
-//       />
-//     </div>
-//   );
-// };
-//
-// export default App;
